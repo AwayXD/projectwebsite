c95690f5dcdfff89db0ab7afd3c5bb019f8e6a1b
Away <SpeakableGameYT@gmail.com>
2025-05-21 07:43:24 -0500

    e

---
+++ index_htm_files/1.js
@@ -0,0 +1,22 @@
+window.local_nivoslider_9_data = [
+{
+ autoscroll: "5",
+ effect: "1"
+},
+{
+ caption: "Caption text...",
+ image: "index_htm_files/4424.jpg"
+},
+{
+ caption: "Caption text...",
+ image: "index_htm_files/4425.jpg"
+},
+{
+ caption: "Caption text...",
+ image: "index_htm_files/4426.jpg"
+},
+{
+ caption: "Caption text...",
+ image: "index_htm_files/4427.jpg"
+}
+];
